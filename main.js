ec26484706ca4334bc9b4c16faca8c1f66a39ae5
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-01-09 11:13:26 +0100

    finish!!

--- main.js
+++ main.js
@@ -2,73 +2,60 @@
 
 const btn = document.querySelector ('.js-btn');
 const userOption = document.querySelector ('.js-option');
-const playResult = document.querySelector ('.js-result');
+const msgResult = document.querySelector ('.js-result');
 const player = document.querySelector ('.js-player');
 const computer = document.querySelector ('.js-pc');
 
 
 
 const playOptions = () => {
-    playResult.innerHTML= userOption.value;
-        
-}; // este es el selector 
+    return userOption.value;
+}; // este es el selector. retorna el valor que el usuario mete pero ya. Luego hace falta una funcion que la guarde su valor para poder usarlo
 
 const getRandom = () => {
     const random = Math.ceil(Math.random()*9);
     return random;
-}//esta funcion consigue el numero aleatorio 
+};//esta funcion consigue el numero aleatorio 
 
 const numberResult = () => {
     const randomNumber = getRandom ()
     if (randomNumber<= 3){
-        return 'piedra';
+        return 'Piedra';
     } if (randomNumber >=7) {
-        return 'papel';
+        return 'Papel';
     }else {
-        return 'tijera'; 
+        return 'Tijera'; 
     } 
 } //funcion que compara el numero random 
 
 const winnerResult = () => {
-    const playOptions = ''
-    const random = numberResult (); //no generar numbero random 
+   /* const playOptions = ''*/
+   const aa = playOptions();
+    const random = numberResult (); 
     console.log(random);
-    if (playOptions === 'piedra' &&  random === 'tijera'){
-        playResult.innerHTML = `Has Ganado!`;
-    }else if (playOptions === 'papel' &&  random === 'piedra'){
-        playResult.innerHTML = `Has Ganado!`;
-    }else if (playOptions === 'tijera' &&  random === 'piedra'){
-        playResult.innerHTML = `Has Ganado!`;
-    }else if (playOptions === random){
-        playResult.innerHTML = `Empate!`;
+    
+    if (aa === 'Piedra' &&  random === 'Tijera'){
+        msgResult .innerHTML = `Has Ganado!`;
+    }else if (aa === 'Papel' &&  random === 'Piedra'){
+        msgResult .innerHTML = `Has Ganado!`;
+    }else if (aa === 'Tijera' &&  random === 'Piedra'){
+        msgResult .innerHTML = `Has Ganado!`;
+    }else if (aa === random){
+        msgResult .innerHTML = `Empate!`;
     }else{
-        playResult.innerHTML = `Has perdido!`;
+        msgResult .innerHTML = `Has perdido!`;
 
     }    
 }; //funcion final 
 
 //funcion para dar las conclusiones 
 
-    /*switch (true){
-        case playOptions === 'piedra && ' numberResult === 'tijera':
-        
-        break;
-    case player === "papel" && computer === "piedra":
-        
-        break;
-    case player === "tijera" && computer === "piedra":
-        break;
-    case player === computer:
-        
-        break;
-    default: 
-        
-    }*/
-
 function handleClick(event) {
     event.preventDefault();
-    playOptions();
+    
     winnerResult();
+    console.log (winnerResult);
+
 }
 
 btn.addEventListener('click', handleClick);
